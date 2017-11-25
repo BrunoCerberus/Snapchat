ae781b1ead7848fe95b8f0b0ba4b6e4962b7a370
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-24 23:34:30 -0200

    Telas de login e criar conta criadas

---
+++ Snapchat/CriarViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CriarViewController.swift
+//  Snapchat
+//
+//  Created by Bruno Lopes de Mello on 24/11/2017.
+//  Copyright © 2017 Bruno Lopes de Mello. All rights reserved.
+//
+
+import UIKit
+
+class CriarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Snapchat/ViewController.swift
+++ Snapchat/ViewController.swift
@@ -14,10 +14,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
     }
 
 
